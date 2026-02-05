16f07d744703da36569ef26c5174903a25ccfee9
Scott Sauber <stsauber@gmail.com>
2026-02-04 22:46:03 -0600

    init

---
+++ src/Todo.Web/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+namespace Todo.Web.Models;
+
+public class TodoItem
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Title { get; set; } = string.Empty;
+    public bool IsCompleted { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? CompletedAt { get; set; }
+}
